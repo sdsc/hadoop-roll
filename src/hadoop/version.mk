520b551618f2251cb0a420450588d22ab26f8ad5
Trevor Cooper <tcooper@sdsc.edu>
2013-11-20 17:25:42 -0800

    Updated Makefile and version.mk for pull.mk

--- src/hadoop/version.mk
+++ src/hadoop/version.mk
@@ -1,3 +1,14 @@
-NAME    = hadoop
-VERSION = 1.1.1
-RELEASE = 0
+NAME               = hadoop
+VERSION            = 1.1.1
+RELEASE            = 0
+PKGROOT            = /opt/hadoop
+
+SRC_SUBDIR         = hadoop
+
+SOURCE_NAME        = $(NAME)
+SOURCE_VERSION     = $(VERSION)
+SOURCE_SUFFIX      = tar.gz
+SOURCE_PKG         = $(SOURCE_NAME)-$(SOURCE_VERSION).$(SOURCE_SUFFIX)
+SOURCE_DIR         = $(SOURCE_PKG:%.$(SOURCE_SUFFIX)=%)
+
+TAR_GZ_PKGS        = $(SOURCE_PKG)
