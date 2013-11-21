21b4534c379a49d778a1a5ba4da62f40bfa3dd21
Trevor Cooper <tcooper@sdsc.edu>
2013-11-20 17:29:29 -0800

    Updated Makefile and version.mk to use pull.mk

--- src/myHadoop/version.mk
+++ src/myHadoop/version.mk
@@ -1,3 +1,14 @@
-NAME    = myHadoop
-VERSION = 0.2a
-RELEASE = 4
+NAME               = myHadoop
+VERSION            = 0.2a
+RELEASE            = 4
+PKGROOT            = /opt/hadoop/contrib/myHadoop
+
+SRC_SUBDIR         = myHadoop
+
+SOURCE_NAME        = $(NAME)
+SOURCE_VERSION     = $(VERSION)
+SOURCE_SUFFIX      = tar.gz
+SOURCE_PKG         = $(SOURCE_NAME)-$(SOURCE_VERSION).$(SOURCE_SUFFIX)
+SOURCE_DIR         = $(SOURCE_PKG:%.$(SOURCE_SUFFIX)=%)
+
+TAR_GZ_PKGS        = $(SOURCE_PKG)
