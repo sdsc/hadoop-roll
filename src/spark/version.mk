d32d22cd25083babac7ad5da7f709b884aa11162
Jim Hayes <jhayes@sdsc.edu>
2015-01-21 15:56:26 -0800

    Upgrade package version.

--- src/spark/version.mk
+++ src/spark/version.mk
@@ -1,6 +1,6 @@
 NAME           = sdsc-spark
-VERSION        = 0.9.1
-RELEASE        = 3
+VERSION        = 1.2.0
+RELEASE        = 0
 PKGROOT        = /opt/spark
 
 SRC_SUBDIR     = spark
@@ -8,7 +8,7 @@ SRC_SUBDIR     = spark
 SOURCE_NAME    = spark
 SOURCE_SUFFIX  = tgz
 SOURCE_VERSION = $(VERSION)
-SOURCE_PKG     = $(SOURCE_NAME)-$(SOURCE_VERSION)-bin-hadoop2.$(SOURCE_SUFFIX)
+SOURCE_PKG     = $(SOURCE_NAME)-$(SOURCE_VERSION)-bin-hadoop2.4.$(SOURCE_SUFFIX)
 SOURCE_DIR     = $(SOURCE_PKG:%.$(SOURCE_SUFFIX)=%)
 
 TGZ_PKGS       = $(SOURCE_PKG)
