2bf2544104e4a1aaa7ce42a38c488e72cb7f15cd
Jerry Greenberg <jpg@comet-devel.local>
2015-03-11 21:32:58 -0700

    addded auto versioning

--- version.mk
+++ version.mk
@@ -1,5 +1,6 @@
-ROLLNAME = hadoop
-RELEASE  = 2
-COLOR    = orange
+ROLLNAME        = hadoop
+VERSION        :=$(shell bash version.sh -v)
+RELEASE        :=$(shell bash version.sh -h)
+COLOR           = orange
 
-REDHAT.ROOT = $(PWD)
+REDHAT.ROOT     = $(PWD)
