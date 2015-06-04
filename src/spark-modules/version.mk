177954150a709620c5dd3e1997e0b8799bd3d9e1
Jerry Greenberg <jpg@comet-devel.sdsc.edu>
2015-06-04 09:54:31 -0700

    fixed spark paths and hadoop version

--- src/spark-modules/version.mk
+++ src/spark-modules/version.mk
@@ -2,7 +2,7 @@ PACKAGE     = spark
 CATEGORY    = applications
 
 NAME        = sdsc-$(PACKAGE)-modules
-RELEASE     = 3
+RELEASE     = 4
 PKGROOT     = /opt/modulefiles/$(CATEGORY)/$(PACKAGE)
 
 VERSION_SRC = $(REDHAT.ROOT)/src/$(PACKAGE)/version.mk
