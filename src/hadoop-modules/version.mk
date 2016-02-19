ed85f9e12ad1c142a0bbc3ab7e22208d9e1701a4
Jerry Greenberg <jpg@comet-01-65.local>
2016-02-19 12:04:38 -0800

    spark module path fixed and added a hadoop modle path

--- src/hadoop-modules/version.mk
+++ src/hadoop-modules/version.mk
@@ -2,13 +2,13 @@ PACKAGE     = hadoop
 CATEGORY    = applications
 
 NAME        = sdsc-$(PACKAGE)-modules
-RELEASE     = 3
+RELEASE     = 4
 PKGROOT     = /opt/modulefiles/$(CATEGORY)/$(PACKAGE)
 
 VERSION_SRC = $(REDHAT.ROOT)/src/$(PACKAGE)/version.mk
 VERSION_INC = version.inc
 include $(VERSION_INC)
 
-EXTRA_MODULE_VERSIONS = 1.1.1 1.2.1
+EXTRA_MODULE_VERSIONS = 1.2.1
 
 RPM.EXTRAS  = AutoReq:No
